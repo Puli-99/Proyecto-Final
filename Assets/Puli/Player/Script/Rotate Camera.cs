2cd038fe7018d6382ac680ada8fc8fffd2825b94
Puli-99 <amador.pablo99@gmail.com>
2025-05-24 19:43:32 -0300

    Rotar cámara y seguimiento de cámara
    Nuevo script(Rotate Camera) en el que si tocas R rota la camara 90º.
    Además introduje CineMachine para seguimiento del player con un angulo, damp y preset específicos

---
+++ Assets/Puli/Player/Script/Rotate Camera.cs
@@ -0,0 +1,54 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] CinemachineFreeLook cinemachine;
+    [SerializeField] float rotationSpeed;
+    bool isRotating;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !isRotating)
+        {
+            StartCoroutine("Rotating");
+        }
+    }
+
+    IEnumerator Rotating()
+    {
+        isRotating = true;
+        float currentValue = cinemachine.m_XAxis.Value;
+        Debug.Log("CV " + currentValue);
+
+        if (currentValue >= 179 && currentValue <= 180)
+        {
+            Debug.Log("valgo -180");
+            currentValue = -179;
+            cinemachine.m_XAxis.Value = -179;
+        }
+        else if(currentValue >= 89 && currentValue <= 95)
+        {
+            currentValue = 89;
+            cinemachine.m_XAxis.Value = 89;
+        }
+
+        while (cinemachine.m_XAxis.Value <= currentValue + 90)
+        {
+            cinemachine.m_XAxis.Value += rotationSpeed * Time.deltaTime;
+            yield return null;
+        }
+        isRotating = false;
+    }
+}
